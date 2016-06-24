19b16e7715b0db77512e15688ead7dddb4a99ab6
Mewlips <mewlips@gmail.com>
2016-06-25 00:41:12 +0900

    fix uninstall

--- install/app/uninstall.sh
+++ install/app/uninstall.sh
@@ -3,10 +3,10 @@
 source /opt/usr/apps/nx-remote-controller-mod/common.sh
 
 if $POPUP_OK "Do you really want to uninstall?" "YES" "NO"; then
-    $APP_DIR/off_nx-remote-controller-daemon.sh
-#    swapoff $APP_DIR/swapfile
-    cp $POUP_TIMEOUT /tmp/
-    rm -rf $APP_DIR
+    $APP_PATH/off_nx-remote-controller-daemon.sh
+#    swapoff $APP_PATH/swapfile
+    cp $POPUP_TIMEOUT /tmp/
+    rm -rf $APP_PATH
     sync; sync; sync
     /tmp/popup_timeout " [ Uninstall completed. ]" 2
     rm -f /tmp/popup_timeout
