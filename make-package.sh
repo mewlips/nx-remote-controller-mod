ad1b71ac2b22791620a0ba41213dca17f74778ba
Mewlips <mewlips@gmail.com>
2016-06-25 00:46:55 +0900

    update install packages

--- make-package.sh
+++ make-package.sh
@@ -4,3 +4,4 @@ ZIP_FILE=nx-remote-controller-mod.zip
 
 rm -f $ZIP_FILE
 (cd install; zip -r ../$ZIP_FILE app/ nx-on-wake/)
+cp -f NXRemoteController/app/build/outputs/apk/app-debug.apk NXRemoteController.apk
