fcf5f0c69a538af65a528875cb16563e98fcd1ac
Mewlips <mewlips@gmail.com>
2016-06-25 00:26:51 +0900

    don't create swapfile

--- install/app/nx-remote-controller-daemon.sh
+++ install/app/nx-remote-controller-daemon.sh
@@ -5,16 +5,16 @@ EXT_APP_PATH=$APP_PATH/externals
 TOOLS_PATH=$APP_PATH/tools
 POPUP_TIMEOUT=$EXT_APP_PATH/popup_timeout
 
-SWAP_FILE=$APP_PATH/swapfile
-if [[ ! -f $SWAP_FILE ]]; then
-    $POPUP_TIMEOUT " [ Initializing swap, please wait... ] " 20 &
-    dd if=/dev/zero of=$SWAP_FILE bs=1024 count=131072 && mkswap $SWAP_FILE
-    killall popup_timeout
-fi
-if free | grep Swap | [ $(awk '{ print $2 }') == 0 ]; then
-    swapon $SWAP_FILE
-    sysctl vm.swappiness=10
-fi
+#SWAP_FILE=$APP_PATH/swapfile
+#if [[ ! -f $SWAP_FILE ]]; then
+#    $POPUP_TIMEOUT " [ Initializing swap, please wait... ] " 20 &
+#    dd if=/dev/zero of=$SWAP_FILE bs=1024 count=131072 && mkswap $SWAP_FILE
+#    killall popup_timeout
+#fi
+#if free | grep Swap | [ $(awk '{ print $2 }') == 0 ]; then
+#    swapon $SWAP_FILE
+#    sysctl vm.swappiness=10
+#fi
 
 killall nx-remote-controller-daemon
 sleep 1
--- install/app/uninstall.sh
+++ install/app/uninstall.sh
@@ -4,7 +4,7 @@ source /opt/usr/apps/nx-remote-controller-mod/common.sh
 
 if $POPUP_OK "Do you really want to uninstall?" "YES" "NO"; then
     $APP_DIR/off_nx-remote-controller-daemon.sh
-    swapoff $APP_DIR/swapfile
+#    swapoff $APP_DIR/swapfile
     cp $POUP_TIMEOUT /tmp/
     rm -rf $APP_DIR
     sync; sync; sync
