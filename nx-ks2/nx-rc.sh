d545778fe31f575341b30c7911a6a9f141007b9b
Mewlips <mewlips@gmail.com>
2016-10-09 20:04:53 +0900

    update nx-rc scripts (by KinoSeed)

--- nx-ks2/nx-rc.sh
+++ nx-ks2/nx-rc.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-
-killall nx-input-injector
-killall xev-nx
-killall nx-remote-controller-daemon
-
+#
+killall -q nx-input-injector
+killall -q xev-nx
+killall -q nx-remote-controller-daemon
+killall -q onscreen_rc
+#
 export APP_PATH=/opt/usr/nx-ks/nx-rc
 nice --adjustment=19 $APP_PATH/nx-remote-controller-daemon &> /dev/null &
+#
+while true; do
+	IP=`ip addr ls|grep inet|grep mlan0|cut -d/ -f 1|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'`
+	[[ -z $IP ]] && IP="Enable WiFi"
+	killall onscreen_rc
+	/opt/usr/nx-ks/onscreen_rc "rc: $IP" &
+	sleep 3
+done
+
--- nx-ks2/off_nx-rc.sh
+++ nx-ks2/off_nx-rc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
-killall nx-input-injector
-killall xev-nx
-killall nx-remote-controller-daemon
+#
+killall -q nx-input-injector
+killall -q xev-nx
+killall -q nx-remote-controller-daemon
+killall -q onscreen_rc
+killall -q nx-rc.sh
