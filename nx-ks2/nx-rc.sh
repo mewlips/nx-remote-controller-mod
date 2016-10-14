71607d55ae2f7ba6b9fa6a49dc23057f819c3834
Mewlips <mewlips@gmail.com>
2016-10-14 23:48:09 +0900

    update nx-rc.sh

--- nx-ks2/nx-rc.sh
+++ nx-ks2/nx-rc.sh
@@ -8,11 +8,14 @@ killall -q onscreen_rc
 export APP_PATH=/opt/usr/nx-ks/nx-rc
 nice --adjustment=19 $APP_PATH/nx-remote-controller-daemon &> /dev/null &
 #
-while true; do
+showmetheway(){
+	while true; do
 	IP=`ip addr ls|grep inet|grep mlan0|cut -d/ -f 1|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'`
 	[[ -z $IP ]] && IP="Enable WiFi"
 	killall onscreen_rc
-	/opt/usr/nx-ks/onscreen_rc "rc: $IP" &
-	sleep 3
-done
-
+	 nice -n +10 /opt/usr/nx-ks/onscreen_rc "rc: $IP" &
+	sleep 4
+	done
+}
+showmetheway&
+#tothenextwhiskeybar
